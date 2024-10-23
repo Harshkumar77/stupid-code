45c28844714ec9cd9d043a10950babb9c1f1a660
Harsh Kumar <72347597+Harshkumar77@users.noreply.github.com>
2024-10-23 18:01:21 +0530

    r

--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
 Math.random = () => crypto.getRandomValues(new Uint32Array(1))[0] / (2**32);
+
+function copyToClipboard(elementId) {
+    const text = document.getElementById(elementId).innerText;
+    navigator.clipboard.writeText(text).then(() => {
+        alert(`Copied: ${text}`);
+    }).catch(err => console.error('Failed to copy text: ', err));
+};
